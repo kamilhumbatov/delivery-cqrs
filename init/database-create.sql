ce9fff57014561a46a8c08a8943f1a8453d7576e
kamilhumbatov <Kamil.Humbatov@unibank.az>
2022-04-04 21:24:45 +0400

    Multi datbase create

---
+++ init/database-create.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `delivery_deliver`;
+GRANT ALL ON `delivery_deliver`.* TO 'delivery'@'%';
+
+CREATE DATABASE IF NOT EXISTS `delivery_security`;
+GRANT ALL ON `delivery_security`.* TO 'delivery'@'%';
